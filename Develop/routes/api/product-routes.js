d22c96c7a9a7c2462e4695dc1e918f1532264a64
Jorge Guzman <jorgeguzman@Jorges-MacBook-Air.local>
2024-07-17 21:10:10 -0400

    wrapping up

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Category and Tag data
 
   try {
-    const productData = await Product.findAll(req.params.id, {
+    const productData = await Product.findAll( {
       include: [
         { model: Category, attributes: ["id", "category_name"] },
         { model: Tag, attributes: ["id", "tag_name"]},
@@ -65,14 +65,14 @@ router.post("/", (req, res) => {
   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        return ProductTag.bulkCreate(productTagIdArr).then(() => res.status(200).json(product));
       }
       // if no product tags, just respond
       res.status(200).json(product);
@@ -128,13 +128,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
+  try {
+    const productData = await Product.destroy({
     where: {
-      id: req.params.id,
+      id: req.params.id
     },
   });
+  if (!productData) {
+    res.status(404).json({ message: "No product found with that id!"});
+    return;
+  }
+  res.status(200).json({ message: "Product deleted"});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # E Commerce Back-End
+
+
+  ## Table of Contents
+- [Description](#description) 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Contact](#contact)
+ 
+
+  ## Description
+
+  The purpose of this project is to create a  database and application backend for an e-commerce site. Built using Postgres, Express, Sequelize and dotenv.
+
+  
+  ## Installation
+
+  To install, clone Github repository and install on local machine.
+
+
+  <video controls src="E-Commerce.mp4" title="Title"></video>
+
+  
+  ## Usage
+
+  For usage, node.js, Inquire, pg npm packages, and postgres sql must be installed, 
+
+  ## Contributing
+  Jorge Guzman
+
+  ## Test
+  Various test were ran using node js.
+  
+  ## Contact
+  Email: jguzman913@gmail.com
+
+  Github: https://github.com/JGuzman87/
